018b9c1213fb0455f801260dc1e0c0fa4108f4b1
Richard Shi <richardshi@hubdoc.com>
2017-10-27 13:19:11 -0400

    Finalized Block design

--- block.js
+++ block.js
@@ -1,32 +1,35 @@
 'use strict';
 
-const Crypto = require('crypto')
-const VERSION = "0.1.0";
+const Crypto = require('crypto');
+const VERSION = '0.1.0';
 
 module.exports = class Block {
-    constructor(timestamp, prev_block_hash, data, nonce) {
-        // Hashed data
-        this.timestamp = timestamp;
-        this.prev_block_hash = prev_block_hash;
-        this.data_hash = hash_data(data);
-        this.nonce = nonce;
-
+    constructor(previous_block, timestamp, data, nonce) {
         // Block metadata
         this.version = VERSION;
-
-        // Data
         this.data = data;
+        this.previous_block = previous_block;
+        
+        // Hashed data
+        this.timestamp = timestamp;
+        this.previous_hash = (previous_block !== 'Genesis') ? this.previous_block.hash() : "0";
+        this.data_hash = hash_data(data);
+        this.nonce = nonce;
     }
 
-    hash(){
-        return Crypto.createHash('sha256').update(this.toString()).digest('hex')
+    hash() {
+        return Crypto.createHash('sha256')
+            .update(this.toString())
+            .digest('hex');
     }
 
-    toString(){
+    toString() {
         return `${this.timestamp}${this.prev_block_hash}${this.data_hash}${this.nonce}`;
     }
 
-    hash_data(data){
-        return Crypto.createHash('sha256').update(data).digest('hex');
+    hash_data(data) {
+        return Crypto.createHash('sha256')
+            .update(data)
+            .digest('hex');
     }
 };
