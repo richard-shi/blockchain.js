f0ef3f7cecb287b502d51dab1d0f2606d0a75abd
Abdulqader Almetwali <abdulqaderalmetwali@hubdoc.com>
2017-10-27 13:16:42 -0400

    it works locally mine+get blocks

--- blockchain.js
+++ blockchain.js
@@ -2,23 +2,20 @@
 
 const Crypto = require('crypto');
 const Block = require('./block').Block;
-
 const DIFFICULTY = 1;
 const BLOCK_COUNT = 20;
 
-// Functions
 const hash_data = function(data){
     return Crypto.createHash('sha256').update(data).digest('hex');
 }
 
 const is_valid = function(block, difficulty = DIFFICULTY){
-    return ((block.prev_hash in chain && block.prev_hash) || block.prev_hash === '0')   // Check chain for previous hash
-    && block.hash().slice(0, difficulty) === '0'.repeat(difficulty);                    // Check if block meets target
+    return block.hash().slice(0, difficulty) === '0'.repeat(difficulty);                    // Check if block meets target
 }
 
 const mine = function(block){
     while(!is_valid(block)){
-        block.header.nonce += 1;
+        block.nonce += 1;
     }
 }
 
@@ -30,34 +27,32 @@ const genesis = function(){
 const create_block = function(data){
     const timestamp = Date.now();
     const data_hash = hash_data(data);
-    const prev_block = chain.last();
+    var prev_hash;
+    var prev_block;
 
-    if(!prev_block){
-        throw new Error("Please create genesis block!")
+    if(chain.length === 0){
+        prev_hash = "0";
+    } else {
+        prev_block = chain[chain.length-1];
+        prev_hash = prev_block.hash();
     }
+    // if(){
+    //     throw new Error("Please create genesis block!")
+    // }
 
-    const prev_hash = prev_block.hash();
-    
-    return new Block(timestamp, prev_hash, data_hash, data, 0)
-}
+    const block = new Block(timestamp, prev_hash, data_hash, data, 0)
 
-const add_block = function(block){
-    if(!is_valid(block)){
-        return new Error("Block is not valid, not adding")
-    }
+    mine(block)
+    chain.push(block);
 
-    // Mine block
-    block.mine();
-    
-    chain[block.hash()] = block;
+    return block;
 }
 
 // Main
-const chain = {};
+const chain = [];
 const genesis_block = genesis();
 
-chain.add_block(genesis_block);
-
-for(let i = 0; i < BLOCK_COUNT; ++i){
-
+module.exports = {
+  chain,
+  create_block
 }
---
+++ client.js
@@ -0,0 +1,29 @@
+// const socket = require('socket.io');
+const app = require('express')()
+const HTTP_PORT = 3001;
+const SOCKET_PORT = 6001;
+const blockchain = require('./blockchain');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post('/mine', (req,res)=>{
+    console.log(req.body);
+    console.log(req.body.transaction)
+    // console.log(req.body.data);
+    // blockchain.create_block(JSON.stringify(req.body.data))
+    // blockchain.push(req.)
+    blockchain.create_block(req.body.transaction)
+    res.send("success")
+})
+
+app.get('/blocks', (req,res) => {
+    console.log("llll")
+    console.log(blockchain.chain);
+    res.send(JSON.stringify(blockchain.chain));
+})
+
+app.listen(HTTP_PORT, function(){
+  console.log(`listening to port ${HTTP_PORT}`)
+})
