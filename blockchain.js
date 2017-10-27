a50773026692760e25fe012411a1e8677b1356c5
Richard Shi <richardshi@hubdoc.com>
2017-10-27 11:32:58 -0400

    Made more changes to blockchain data structure

--- blockchain.js
+++ blockchain.js
@@ -11,38 +11,49 @@ const hash_data = function(data){
     return Crypto.createHash('sha256').update(data).digest('hex');
 }
 
-const is_valid_block = function(block, difficulty = DIFFICULTY){
-    return block.hash().slice(0, difficulty) === '0'.repeat(difficulty);
+const is_valid = function(block, difficulty = DIFFICULTY){
+    return ((block.prev_hash in chain && block.prev_hash) || block.prev_hash === '0')   // Check chain for previous hash
+    && block.hash().slice(0, difficulty) === '0'.repeat(difficulty);                    // Check if block meets target
 }
+
 const mine = function(block){
-    while(!valid(block)){
+    while(!is_valid(block)){
         block.header.nonce += 1;
     }
 }
 
 const genesis = function(){
     const data = "Shinji get in the robot"
-
-
-    return block
+    return create_block(data)
 }
 
 const create_block = function(data){
+    const timestamp = Date.now();
     const data_hash = hash_data(data);
-    const prev_hash = 
+    const prev_block = chain.last();
 
+    if(!prev_block){
+        throw new Error("Please create genesis block!")
+    }
 
+    const prev_hash = prev_block.hash();
+    
+    return new Block(timestamp, prev_hash, data_hash, data, 0)
 }
 
 const add_block = function(block){
+    if(!is_valid(block)){
+        return new Error("Block is not valid, not adding")
+    }
 
+    chain.
 }
 
 // Main
-const chain = [];
+const chain = {};
 const genesis_block = genesis();
 
-chain.push(genesis_block)
+chain.add_block(genesis_block)
 
 for(let i = 0; i < BLOCK_COUNT; ++i){
 
